eeb1aae6cdccd81ecac38c0e9778dbc9c1354b37
YaoLei <yaolei@yunwii.com>
2017-10-20 11:05:03 +0800

    删除弹框

--- 12306/js/12306.js
+++ 12306/js/12306.js
@@ -9,14 +9,14 @@ img.onclick = function exchange() {
 
 }
 
-var setOutData = document.getElementById('set-out-data');
-setOutData.onfocus = function showOutData() {
-    document.getElementById("toublecalender").style.display = "block";
-}
+// var setOutData = document.getElementById('set-out-data');
+// setOutData.onfocus = function showOutData() {
+//     document.getElementById("toublecalender").style.display = "block";
+// }
 
-var toublecalenderCloseImg = document.getElementById("toublecalender-close-img");
-toublecalenderCloseImg.onclick = function closeOutData() {
-        document.getElementById("toublecalender").style.display = "none";
+// var toublecalenderCloseImg = document.getElementById("toublecalender-close-img");
+// toublecalenderCloseImg.onclick = function closeOutData() {
+//         document.getElementById("toublecalender").style.display = "none";
     }
     // toublecalenderCloseImg.addEventListener('click',closeOutData,false);
     // var gradient = document.getElementById("gradient");
